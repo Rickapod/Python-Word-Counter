2de2cba5d25c772304a616ac10a0e4379b06f6e3
Rick <buddgarcia@gmail.com>
2020-01-29 23:07:02 -0800

    implemented a word count list to be shown to the user and created an about page

--- wordcounter-project/wordcounter/views.py
+++ wordcounter-project/wordcounter/views.py
@@ -1,9 +1,13 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
 
 def count(request):
   fulltext = request.GET['fulltext']
@@ -19,10 +23,14 @@ def count(request):
     else:
       #add to the dictionare
       words[word] = 1
-  mostword = ''
-  mostwordcount = 0
-  for word in words:
-    if words[word] > mostwordcount:
-      mostwordcount = words[word]
-      mostword = word
-  return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordcount), 'mostword': mostword })
+  
+
+  sortedwords = sorted(words.items(), key=operator.itemgetter(1), reverse = True)
+  mostword = sortedwords[0][0]
+  mostwordcount = sortedwords[0][1]
+
+  # for word in words:
+  #   if words[word] > mostwordcount:
+  #     mostwordcount = words[word]
+  #     mostword = word
+  return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordcount), 'mostword': mostword, 'mostwordcount': mostwordcount, 'sortedwords': sortedwords })
