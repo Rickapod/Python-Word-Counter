ad71bad1e74c5c6cf101181b7f9d0055584b4a2d
Rick <buddgarcia@gmail.com>
2020-01-29 18:48:35 -0800

    implemented a function to display the most common word in the user provided text

--- wordcounter-project/wordcounter/views.py
+++ wordcounter-project/wordcounter/views.py
@@ -7,5 +7,22 @@ def homepage(request):
 
 def count(request):
   fulltext = request.GET['fulltext']
+  
   wordcount = fulltext.split()
-  return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordcount) })
+
+  words = {}
+
+  for word in wordcount:
+    if word in words:
+      #increase count
+      words[word] += 1
+    else:
+      #add to the dictionare
+      words[word] = 1
+  mostword = ''
+  mostwordcount = 0
+  for word in words:
+    if words[word] > mostwordcount:
+      mostwordcount = words[word]
+      mostword = word
+  return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordcount), 'mostword': mostword })
